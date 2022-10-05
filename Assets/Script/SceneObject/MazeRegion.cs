e683f204dca97b0ea03cd3352dd2b8049e4102b5
szlx560223 <764345063@qq.com>
2022-10-05 22:42:51 +0800

    完成第一关

--- Assets/Script/SceneObject/MazeRegion.cs
+++ Assets/Script/SceneObject/MazeRegion.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class MazeRegion : MonoBehaviour
 {
-    private Vector3 finalPos = new Vector3(0,0,-1);
+    private Vector3 finalPos = new Vector3(0,0,-5);
     private Vector3 currentPos = new Vector3(0, 0, 0);
     public float moveSpeed = 5f;//移动位置
-    private Transform cam = Camera.main.transform;//相机节点
+    private Transform cam;
     private float currentZ; //与屏幕的距离
 
     void OnTriggerEnter2D(Collider2D other)
     { 
         if (other.CompareTag("Player")){
-            finalPos.z = -7;
+            finalPos.z = -10;
         }
     }
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            finalPos.z = -3;
+            finalPos.z = -5;
         }
     }
     void Start()
     {
+        cam = Camera.main.transform;//相机节点
         currentZ = finalPos.z;
     }
     void Update()
---
+++ Assets/Script/UI/TriggerAtStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAtStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextManager.instance.SetText("self", "今天的苹果真好吃！该找个地方丢垃圾了。");
+        TextManager.instance.SetText("self", "哪里有丢垃圾的地方呢？去问问前面的那位阿姨吧！");
+    }
+
+
+}
